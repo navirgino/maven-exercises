5752c19e923aae56c334968db0b9b0b8f6ffe5f0
Nico Virgino <nicovirgino@gmail.com>
2019-12-12 16:53:59 -0600

    exercises

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,24 @@
 import org.apache.commons.lang3.StringUtils;
 
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Input input = new Input();
 
         System.out.println("PUT IN A NUMBER BANANA: ");
-        String input = sc.nextLine();
-        System.out.println(StringUtils.isNumeric(input));
+
+        System.out.println(StringUtils.isNumeric(input.getString()));
         System.out.println("input some stuff: ");
-        input = sc.nextLine();
-//        System.out.println(StringUtils.swapCase(input));
-        System.out.println(StringUtils.getLevenshteinDistance(input, input, 4));
+//        input = sc.nextLine();
+        System.out.println(StringUtils.swapCase(input.getString()));
+//        System.out.println(StringUtils.getLevenshteinDistance(input, input, 4));
 //        System.out.println(input);
+
+        System.out.println("Enter some other thing: ");
+        System.out.println(StringUtils.reverse(input.getString()));
 
 
     }
